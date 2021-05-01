2048bdcc95e261abd068f4e22a6640c6ddbffb4f
Marcelo Machado Correa Junior <marcelomcj777@gmail.com>
2021-05-01 15:25:27 -0300

    primeiro comit

---
+++ src/main/java/br/com/usj/programacaoweb/calculadorahistorico/CalculadoraHistoricoApplication.java
@@ -0,0 +1,13 @@
+package br.com.usj.programacaoweb.calculadorahistorico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraHistoricoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraHistoricoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/usj/programacaoweb/calculadorahistorico/CalculadoraHistoricoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.usj.programacaoweb.calculadorahistorico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraHistoricoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
