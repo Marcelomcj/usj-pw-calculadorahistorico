254520d5554f365062eeca0972cdfafe0e62ae9c
Marcelo Machado Correa Junior <marcelomcj777@gmail.com>
2021-05-15 13:54:24 -0300

    camada de persistencia (entity e repository)

---
+++ src/main/java/br/com/usj/programacaoweb/calculadorahistorico/OperacaoRepository.java
@@ -0,0 +1,7 @@
+package br.com.usj.programacaoweb.calculadorahistorico;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OperacaoRepository extends CrudRepository<Operacao, Long> {
+    
+}
